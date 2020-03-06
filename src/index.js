290775eaf99b5ea3afa5703e22913a0279709c6f
chelingliao <jessie910916@gmail.com>
2020-03-06 16:54:01 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 serviceWorker.unregister();
