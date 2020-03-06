87db7d188fcfc90ea5664832bc46742b46edd3f4
chelingliao <jessie910916@gmail.com>
2020-03-06 17:10:03 +0800

    second

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,10 +14,11 @@ class App extends Component {
     } 
 
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => { this.setState({ robots: users })})
-    }
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => {this.setState({ robots: users})});
+      }
+    
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
